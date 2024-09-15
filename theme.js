044092c34b4f4cffb60fbafc547c7402771c8f93
Lingfengyu <1378990254@qq.com>
2024-09-15 16:25:41 +0800

    feature: 完成配置面板

--- theme.js
+++ theme.js
@@ -1,7 +1,6 @@
 // 主题默认加载时进行的行为
 // js代码加载后立即执行
 (function () {
-    // const body = document.getElementsByTagName("body")[0];
     // 读取配置文件或生成配置文件
     getSettings();
     addThemeToolBar();
@@ -62,17 +61,6 @@ async function _rqFORSiyuan(url, data) {
         return null;
 }
 
-/**
- * @Description 
- * @Feature 解析思源的响应
- * @param response
- * @return data or null
- */
-async function _analyseResponse(response) {
-    let r = await response;
-    return r.code === 0 ? r.data : null;
-}
-
 /** 
  * @Feature 获取文件
  * @param path 文件路径
@@ -86,21 +74,6 @@ async function _getFile(path, then = null, obj = null) {
     }).then((v) => {
         if (then)
             then(v, obj);
-    });
-}
-
-/**
- * @Description 根据路径获取人类可读路径
- * @Feature 
- * @param noteBook 笔记本id
- * @param path 路径
- * @return 
- */
-async function _getReadablePath(noteBook, path) {
-    let url = '/api/filetree/getHPathByPath';
-    await _rqFORSiyuan(url, {
-        notebook: noteBook,
-        path: path
     });
 }
 
@@ -133,9 +106,22 @@ async function _writeFile(path, filedata, then = null, obj = null, isDir = false
     }, 200);
 }
 
+/**
+ * @Feature 发送消息
+ * @param type 消息类型
+ * @param message 消息内容
+ * @param time 持续时间
+ */
+async function _postMessage(type, message, time = null) {
+    if (type == "ok") url = "/api/notification/pushMsg";
+    else if (type == "error") url = "api/notification/pushErrMsg";
+    if (time) await _rqFORSiyuan(url, { "msg": message, "timeout": time });
+    else await _rqFORSiyuan(url, { "msg": message });
+}
+
 async function getSettings() {
     // var res = _analyseResponse(_getFile("/data/snippets/vsc_edit.config.json"));
-    _getFile("/data/snippets/vsc_edit.config.json", (v) => {
+    await _getFile("/data/snippets/vsc_edit.config.json", (v) => {
         if (v == null) {
             v = defaultConf;
             putSettings(v);
@@ -148,15 +134,10 @@ async function getSettings() {
 }
 
 async function putSettings(settings) {
-    _writeFile("/data/snippets/vsc_edit.config.json", JSON.stringify(settings), null, null, false, Date.now());
-}
-
-/**
- * 得到思源toolbar
- * @returns 
- */
-function getSiYuanToolbar() {
-    return document.getElementById("toolbar");
+    if (settings == null) {
+        return;
+    }
+    await _writeFile("/data/snippets/vsc_edit.config.json", JSON.stringify(settings), null, null, false, Date.now());
 }
 
 /**
@@ -165,69 +146,6 @@ function getSiYuanToolbar() {
  */
 function getvscleToolbar() {
     return document.getElementById("vscleToolbar");
-}
-
-/**简单判断目前思源是否是pc窗口模式 */
-function isPcWindow() {
-    return document.body.classList.contains("body--window");
-}
-
-/**简单判断目前思源是否是手机模式 */
-function isPhone() {
-    return document.getElementById("editor");
-}
-
-/**
- * 向指定父级创建追加一个子元素，并可选添加ID,
- * @param {Element} fatherElement 
- * @param {string} addElementTxt 要创建添加的元素标签
- * @param {string} setId 
- * @returns addElementObject
- */
-function addinsertCreateElement(fatherElement, addElementTxt, setId = null) {
-    if (!fatherElement) console.error("指定元素对象不存在！");
-    if (!addElementTxt) console.error("未指定字符串！");
-    var element = document.createElement(addElementTxt);
-    if (setId) element.id = setId;
-    fatherElement.appendChild(element);
-    return element;
-}
-
-/**
- * 向指定元素后创建插入一个元素，可选添加ID
- * @param {Element} targetElement 目标元素
- * @param {String} addElementTxt 要创建添加的元素标签
- * @param {String} setId 为创建元素设置ID
- */
-function insertCreateAfter(targetElement, addElementTxt, setId = null) {
-    if (!targetElement) console.error("指定元素对象不存在！");
-    if (!addElementTxt) console.error("未指定字符串！");
-    var element = document.createElement(addElementTxt);
-    if (setId) element.id = setId;
-    var parent = targetElement.parentNode;//得到父节点
-    if (parent.lastChild === targetElement) {
-        //如果最后一个子节点是当前元素那么直接添加即可
-        parent.appendChild(element);
-        return element;
-    } else {
-        parent.insertBefore(element, targetElement.nextSibling);//否则，当前节点的下一个节点之前添加
-        return element;
-    }
-}
-
-/**
- * 向指定元素前创建插入一个元素，可选添加ID
- * @param {*} targetElement 目标元素
- * @param {*} addElementTxt 要创建添加的元素标签
- * @param {*} setId 为创建元素设置ID
- */
-function insertCreateBefore(targetElement, addElementTxt, setId = null) {
-    if (!targetElement) console.error("指定元素对象不存在！");
-    if (!addElementTxt) console.error("未指定字符串！");
-    var element = document.createElement(addElementTxt);
-    if (setId) element.id = setId;
-    targetElement.parentElement.insertBefore(element, targetElement);
-    return element;
 }
 
 /**
@@ -253,18 +171,8 @@ function addThemeToolBar() {
     vscToolBar.innerHTML = "VC";
     // vscToolBar.innerHTML = "<img src=\"resources\\h6.bmp\"\\>";
     vscToolBar.addEventListener("click", (event) => {
-        // window.open(_getFile("conf/appearance/themes/siyuan-vscodelite-edit/resources/settings.html"));
-        // window.screenX = event.screenX;
-        // window.screenY = event.screenY;
-        alert("抱歉，暂时无法打开配置文件。请打开工作空间下的/data/snippets/vsc_edit.config.json文件编辑并重新加载思源");
-        var re = confirm("是否要重置配置文件?Reset configuration file?");
-        if (re == true) {
-            v = defaultConf;
-            putSettings(v);
-            alert("重置配置文件成功.Successfully reset configuration file.");
-        } else {
-
-        }
+        // 调用函数创建设置窗口
+        createSettingsWindow();
     });
 }
 
@@ -301,4 +209,193 @@ function showElementSettings(settings) {
         str += "scPanel/";
     }
     return str;
+}
+
+// 创建一个包含标签和复选框的 HTML 结构
+async function createSettingsWindow() {
+    // 创建设置窗口大框
+    const dialogSetting = document.createElement('div');
+    dialogSetting.setAttribute("data-key", "dialog-setting");
+    dialogSetting.classList = "b3-dialog--open";
+    document.body.appendChild(dialogSetting);
+
+    // 创建一个遮罩层
+    const dialog = document.createElement('div');
+    dialog.classList = "b3-dialog";
+    dialog.style.zIndex = '14';
+    dialogSetting.appendChild(dialog);
+
+    // 可关闭遮罩层
+    const scrim = document.createElement('div');
+    scrim.classList = "b3-dialog__scrim";
+    scrim.onclick = () => {
+        closeNotSave();
+    };
+    dialog.appendChild(scrim);
+
+    // 创建窗口容器
+    const dialogContainer = document.createElement('div');
+    dialogContainer.classList = "b3-dialog__container";
+    dialogContainer.style.width = '60vw';
+    dialogContainer.style.height = '60vh';
+    dialogContainer.style.maxWidth = '1280px';
+    dialog.appendChild(dialogContainer);
+
+    // 创建设置窗口
+    const dialogBody = document.createElement('div');
+    dialogBody.classList = 'b3-dialog__body';
+    dialogBody.setAttribute("vslite", "SettingPanel");
+    dialogContainer.appendChild(dialogBody);
+
+
+    // 创建标题
+    const title = document.createElement('h2');
+    title.textContent = 'VSCode Lite Edit设置';
+    title.setAttribute("data-subtype", "h2");
+    // title.setAttribute("data-type", "NodeHeading");
+    title.classList = "h2";
+    dialogBody.appendChild(title);
+
+    // 获取设置文件数组
+    async function fetchSettingsArray() {
+        let re;
+        await _getFile("/data/snippets/vsc_edit.config.json", async (v) => {
+            // console.log(v);
+            if (v == null) {
+                v = defaultConf;
+            }
+            // return v;
+            re = await getSettingArrays(v);
+        });
+        // console.log(re);
+        return re;
+
+        async function getSettingArrays(v) {
+            let settings = [];
+            if (v["theme"]["codeBlock"] == true) {
+                settings.push({ label: '代码块样式', id: 'codeBlock', enable: true });
+            } else {
+                settings.push({ label: '代码块样式', id: 'codeBlock', enable: false });
+            }
+            // 引用
+            if (v["theme"]["reference"] == true) {
+                settings.push({ label: '引用块样式', id: 'referenceBlock', enable: true });
+            } else {
+                settings.push({ label: '引用块样式', id: 'referenceBlock', enable: false });
+            }
+            // 集市
+            if (v["theme"]["bazaar"] == true) {
+                settings.push({ label: '集市样式', id: 'bazaarStyle', enable: true });
+            } else {
+                settings.push({ label: '集市样式', id: 'bazaarStyle', enable: false });
+            }
+            // 嵌入块
+            if (v["theme"]["embeddedBlock"] == true) {
+                settings.push({ label: '嵌入块样式', id: 'embeddedBlock', enable: true });
+            } else {
+                settings.push({ label: '嵌入块样式', id: 'embeddedBlock', enable: false });
+            }
+            // 标题
+            if (v["theme"]["title"] == true) {
+                settings.push({ label: '标题块样式', id: 'titleBlock', enable: true });
+            } else {
+                settings.push({ label: '标题块样式', id: 'titleBlock', enable: false });
+            }
+            // 快捷键面板
+            if (v["plugins"]["shortcutPanel"] == true) {
+                settings.push({ label: '（插件）快捷键面板样式', id: 'scPanelStyle', enable: true });
+            } else {
+                settings.push({ label: '（插件）快捷键面板样式', id: 'scPanelStyle', enable: false });
+            }
+            return settings;
+        }
+    }
+
+    // 创建标签和复选框
+    const settings = await fetchSettingsArray();
+    // console.log(settings);
+
+    // 遍历数组添加选项
+    settings.forEach(setting => {
+        const label = document.createElement('span');
+        label.textContent = setting.label;
+        label.htmlFor = setting.id;
+        label.classList = "fn__flex-1";
+
+        const space = document.createElement('span');
+        space.classList = 'fn__space';
+
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.id = setting.id;
+        checkbox.checked = setting.enable;
+        checkbox.classList = "b3-switch fn__flex-center vslite_sets";
+
+        const div = document.createElement('label');
+        div.classList = "fn__flex b3-label";
+        div.appendChild(label);
+        div.appendChild(space);
+        div.appendChild(checkbox);
+
+        dialogBody.appendChild(div);
+    });
+
+    async function closeAndSave() {
+        saveSt = defaultConf;
+        ckb = document.getElementsByClassName("vslite_sets");
+        Array.from(ckb).forEach(checkbox => {
+            const id = checkbox.id;
+            const ck = checkbox.checked;
+            if (id == "codeBlock") {
+                saveSt["theme"]["codeBlock"] = ck;
+            } else if (id == "referenceBlock") {
+                saveSt["theme"]["reference"] = ck;
+            } else if (id == "referenceBlock") {
+                saveSt["theme"]["reference"] = ck;
+            } else if (id == "bazaarStyle") {
+                saveSt["theme"]["bazaar"] = ck;
+            } else if (id == "embeddedBlock") {
+                saveSt["theme"]["embeddedBlock"] = ck;
+            } else if (id == "titleBlock") {
+                saveSt["theme"]["title"] = ck;
+            } else if (id == "scPanelStyle") {
+                saveSt["plugins"]["shortcutPanel"] = ck;
+            }
+        });
+        await putSettings(saveSt);
+        _postMessage("ok", "配置保存成功，稍后自动刷新");
+        setTimeout(() => { window.location.reload(); }, 2000);
+        document.body.removeChild(dialogSetting);
+    }
+    function closeNotSave() {
+        _postMessage("error", "配置未保存");
+        document.body.removeChild(dialogSetting);
+    }
+
+    // 创建关闭按钮
+    const saveButton = document.createElement('button');
+    saveButton.textContent = '保存';
+    saveButton.classList = "b3-button b3-button--outline fn__flex-center fn__size200";
+    saveButton.onclick = () => {
+        closeAndSave();
+    };
+    const notSaveButton = document.createElement('button');
+    notSaveButton.textContent = '不保存';
+    notSaveButton.classList = "b3-button b3-button--outline fn__flex-center fn__size200";
+    notSaveButton.onclick = () => {
+        closeNotSave();
+    };
+    const label = document.createElement('span');
+    label.textContent = "直接关闭不保存哦，必须点击保存按钮";
+    label.classList = "fn__flex-1";
+    const space = document.createElement('span');
+    space.classList = 'fn__space';
+    const div = document.createElement('label');
+    div.classList = "fn__flex b3-label";
+    div.appendChild(label);
+    div.appendChild(space);
+    div.appendChild(saveButton);
+    div.appendChild(space);
+    div.appendChild(notSaveButton);
+    dialogBody.appendChild(div);
 }
---
+++ unUsedCode.js
@@ -0,0 +1,96 @@
+/**
+ * @Description 根据路径获取人类可读路径
+ * @Feature 
+ * @param noteBook 笔记本id
+ * @param path 路径
+ * @return 
+ */
+async function _getReadablePath(noteBook, path) {
+    let url = '/api/filetree/getHPathByPath';
+    await _rqFORSiyuan(url, {
+        notebook: noteBook,
+        path: path
+    });
+}
+
+/**简单判断目前思源是否是pc窗口模式 */
+function isPcWindow() {
+    return document.body.classList.contains("body--window");
+}
+
+/**简单判断目前思源是否是手机模式 */
+function isPhone() {
+    return document.getElementById("editor");
+}
+
+/**
+ * 向指定父级创建追加一个子元素，并可选添加ID,
+ * @param {Element} fatherElement 
+ * @param {string} addElementTxt 要创建添加的元素标签
+ * @param {string} setId 
+ * @returns addElementObject
+ */
+function addinsertCreateElement(fatherElement, addElementTxt, setId = null) {
+    if (!fatherElement) console.error("指定元素对象不存在！");
+    if (!addElementTxt) console.error("未指定字符串！");
+    var element = document.createElement(addElementTxt);
+    if (setId) element.id = setId;
+    fatherElement.appendChild(element);
+    return element;
+}
+
+/**
+ * 向指定元素后创建插入一个元素，可选添加ID
+ * @param {Element} targetElement 目标元素
+ * @param {String} addElementTxt 要创建添加的元素标签
+ * @param {String} setId 为创建元素设置ID
+ */
+function insertCreateAfter(targetElement, addElementTxt, setId = null) {
+    if (!targetElement) console.error("指定元素对象不存在！");
+    if (!addElementTxt) console.error("未指定字符串！");
+    var element = document.createElement(addElementTxt);
+    if (setId) element.id = setId;
+    var parent = targetElement.parentNode;//得到父节点
+    if (parent.lastChild === targetElement) {
+        //如果最后一个子节点是当前元素那么直接添加即可
+        parent.appendChild(element);
+        return element;
+    } else {
+        parent.insertBefore(element, targetElement.nextSibling);//否则，当前节点的下一个节点之前添加
+        return element;
+    }
+}
+
+/**
+ * 向指定元素前创建插入一个元素，可选添加ID
+ * @param {*} targetElement 目标元素
+ * @param {*} addElementTxt 要创建添加的元素标签
+ * @param {*} setId 为创建元素设置ID
+ */
+function insertCreateBefore(targetElement, addElementTxt, setId = null) {
+    if (!targetElement) console.error("指定元素对象不存在！");
+    if (!addElementTxt) console.error("未指定字符串！");
+    var element = document.createElement(addElementTxt);
+    if (setId) element.id = setId;
+    targetElement.parentElement.insertBefore(element, targetElement);
+    return element;
+}
+
+/**
+ * @Description 
+ * @Feature 解析思源的响应
+ * @param response
+ * @return data or null
+ */
+async function _analyseResponse(response) {
+    let r = await response;
+    return r.code === 0 ? r.data : null;
+}
+
+/**
+ * 得到思源toolbar
+ * @returns 
+ */
+function getSiYuanToolbar() {
+    return document.getElementById("toolbar");
+}
